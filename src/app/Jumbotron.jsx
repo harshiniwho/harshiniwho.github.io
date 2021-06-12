8773e737bf188917505b3aeafc99831ba5cc7aaa
harshiniwho <harshiniramanujam@gmail.com>
2021-06-12 14:43:18 +0530

    fix home

--- src/app/Jumbotron.jsx
+++ src/app/Jumbotron.jsx
@@ -1,38 +1,34 @@
 import React from 'react';
 import { makeStyles, Typography } from '@material-ui/core';
+
+import content from './commons/content';
+
 import faceImg from '../assets/face-circle.png';
 
 function Jumbotron() {
     const useStyles = makeStyles({
         root: {
-            // float: 'right',
-            position: 'relative',
-            left: '-50%', /* or right 50% */
             textAlign: 'center',
             fontFamily: "'Old Standard TT', serif",
         },
         name: {
-            position: 'relative',
             textAlign: 'left',
-            backgroundColor: 'black',
-            color: 'white',
-            width: '25rem',
+            color: 'black',
+            width: '30rem',
             height: '7rem',
             boxShadow: '1rem',
             fontFamily: "'Lato', serif"
         },
         content: {
-            position: 'relative',
-            left: '50%',
             display: 'inline-block',
             zIndex: '700',
-            margin: '0 0 0 0',
+            margin: '12rem 0 0rem 0',
         },
         img: {
+            float: 'left',
+            left: '2%',
             position: 'relative',
-            left: '50%',
             width: '100vh',
-            marginRight:'-8rem',
             marginTop: '5rem',
             objectFit: 'cover'
         },
@@ -47,12 +43,10 @@ function Jumbotron() {
             <img className={classes.img} src={faceImg}></img>
             <div className={classes.content}>
                 <Typography className={classes.name} variant={"h2"}>
-                    <b> &nbsp; Harshini R. &nbsp; </b>
+                    {content.opening}
                 </Typography>
-                <Typography className={classes.designations} variant={"h6"}>
-                    &emsp;&emsp; &emsp; &emsp; Software Engineer <br />
-                    &emsp;&emsp; &emsp; &emsp; Writer <br />
-                    &emsp;&emsp; &emsp; &emsp; Feminist <br />
+                <Typography className={classes.name} variant={"h6"}>
+                    {content.about}
                 </Typography>
             </div>
         </div>
---
+++ src/app/commons/content.js
@@ -0,0 +1,6 @@
+const content = {
+    opening: 'Hi, I am Harshini.',
+    about: "I am a software engineer currently working at Visa Inc. I have had experience working on a little bit of everything in the domain of computer science, primarily being a backend engineer. When I am not stealing code from stackoverflow, I am usually lost in a book or trying to capture my world in words. \n Check out my feminism corner for stories I think you should hear about. Feel free to reach out to me on the intersection of women, coding and books!"
+};
+
+export default content;
